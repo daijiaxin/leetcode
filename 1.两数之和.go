bf2bc89ace3d92ba08d1d907adcd1a4e4a84b4b0
daijiaxin <253138265@qq.com>
2022-03-21 22:45:52 +0800

    2 doc at 2022.03.21

---
+++ 1.两数之和.go
@@ -0,0 +1,24 @@
+package leetcode
+
+/*
+ * @lc app=leetcode.cn id=1 lang=golang
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+func twoSum(nums []int, target int) []int {
+	var result []int
+	for i := 0; i < len(nums)-1; i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				result = append(result, i)
+				result = append(result, j)
+				return result
+			}
+		}
+	}
+	return result
+}
+
+// @lc code=end
